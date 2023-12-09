199845c7728eeb8cbd1016ed6a43e046d02ae403
npokpongkiat <npokpongkiat@gmail.com>
2023-12-08 22:27:40 -0800

    Initial Commit

---
+++ memory/memoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoryApp.swift
+//  memory
+//
+//  Created by Nathan Pokpongkiat on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct memoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
